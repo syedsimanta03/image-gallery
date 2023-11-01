5e5779fade12570a5c47838e2f768f73c643abb5
syedsimanta03 <syed.simanta10@gmail.com>
2023-11-01 12:14:49 +0600

    cleanup

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,29 +1,14 @@
 import React from 'react'
-import { useDrag, useDrop } from 'react-dnd'
 
 const Image = ({ image, isLarge, onSelect, index, moveImage }) => {
   
-  const [active, setActive] = React.useState('')
+
   
   const handleCheckboxToggle = (e) => {
     e.stopPropagation() // Prevent the image's click event from firing
     onSelect(image.id)
   }
 
-  const [, ref] = useDrag({
-    type: 'IMAGE',
-    item: { index },
-  })
-
-  const [, drop] = useDrop({
-    accept: 'IMAGE',
-    hover: (draggedItem) => {
-      if (draggedItem.index !== index) {
-        moveImage(draggedItem.index, index)
-        draggedItem.index = index // Ensure the dragged item index is updated
-      }
-    },
-  })
 
   return (
     <div
@@ -31,7 +16,7 @@ const Image = ({ image, isLarge, onSelect, index, moveImage }) => {
       className={`image ${isLarge ? 'large-image' : ''}`}
     >
       <input type='checkbox' checked={image.isSelected} readOnly />
-      <div ref={(node) => ref(drop(node))}>
+      <div>
         <img
           className={`${image.isSelected ? 'img-active' : ''}`}
           src={image.url}
